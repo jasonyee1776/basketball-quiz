e932eca692225b1fcaf317107a1b77cc97c1aa66
Jason Yee <jasonyee1776@gmail.com>
2018-05-08 13:07:22 -0700

    updated styling and functionality

--- js/main.js
+++ js/main.js
@@ -68,13 +68,17 @@ for (var i = 0; i < content.length; i++) {
 	console.log(questionEl);
 	var answerEl = document.createElement("input");
 	answerEl.id = "a" + i;
+	// add Bootstrap classes to make RWD and mobile friendly
+	questionEl.classList.add("my-3", "col-sm-12");
+	// add Bootstrap classes to make RWD and mobile friendly
+	answerEl.classList.add("my-3" , "col-sm-12");
 	contentEl.appendChild(answerEl);
 }
 
 // append parent div to body
 document.body.appendChild(contentEl);
 
-// loop questions to be displayed onto docuemnt 
+// loop questions to be DISPLAYED onto docuemnt 
 for (var i=0; i<content.length; i++) {
 	var getQuestions = document.getElementById("q"+i);
 	getQuestions.innerHTML = content[i].question;
@@ -89,19 +93,45 @@ submitButton.className = "btn, btn-success";
 submitButton.textContent = "Submit Quiz";
 document.body.appendChild(submitButton);
 
-// Add event listener for submit btn and loop correct answers
+// Count right and wrong answers when submit button is clicked 
 submitButton.addEventListener ("click", function() {
+	// counter for right and wrong answer
+	var right = 0;
+	var wrong = 0;
+
 	for (var i=0; i<content.length; i++) {
-		var getAnswers = document.getElementById("a"+i).value;
-		if (getAnswers.toLowerCase()===content[i].answer) {
+		var getInput = document.getElementById("a"+i).value;
+		var answers = content[i].answer;
+		var questions = document.getElementById("q"+[i]);
+		if (answers.toLowerCase()===getInput.toLowerCase()) {
 			//style question green
-			document.getElementById("q"+[i]).className = "right";
+			questions.classList.add("right", "my-3", "col-sm-12");
+			questions.classList.remove("wrong");
+			right++;
 		} else {
 			//style question red
-			document.getElementById("q"+[i]).className = "wrong";
-		}
-	}
+			questions.classList.add("wrong", "my-3", "col-sm-12");
+			questions.classList.remove("right");
+			wrong++;
+		};
+		console.log(right);
+		document.getElementById("right").textContent = right;
+		document.getElementById("wrong").textContent = wrong;
+	}	
 })
+
+///////////////////// Style document with JS ///////////////////////
+// add background image
+var body = document.body
+body.style.backgroundImage = "url('img/hardwood.jpg')";
+// add margins
+body.classList.add("m-4");
+
+
+// 
+
+/////////// Add Bootstrap to make RWD and mobile-friendly  ////////
+
 
 
 
